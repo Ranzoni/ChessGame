1aa29b65f6f7230de4ba9bcb7a08c3e6742f2214
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2021-02-28 16:48:53 -0300

    Criação da peça Cavalo

---
+++ Back-end/ChessGame/Entities/Knight.cs
@@ -0,0 +1,28 @@
+﻿using ChessGame.Domain.Enums;
+using ChessGame.Domain.Shared;
+using ChessGame.Domain.Structs;
+using System;
+
+namespace ChessGame.Domain.Entities
+{
+    public class Knight : Piece
+    {
+        public Knight(Position position, EColor color, Board board) : base(position, color, board)
+        {
+        }
+
+        protected override bool ValidMove(Position newPosition)
+        {
+            if (!base.ValidMove(newPosition))
+                return false;
+
+            if (Math.Abs(Position.DifferenceColumn(newPosition)) == 2 && Math.Abs(Position.DifferenceLine(newPosition)) == 1)
+                return true;
+
+            if (Math.Abs(Position.DifferenceColumn(newPosition)) == 1 && Math.Abs(Position.DifferenceLine(newPosition)) == 2)
+                return true;
+
+            return false;
+        }
+    }
+}
